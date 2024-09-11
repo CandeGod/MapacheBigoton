902c6b9ba13944bff0174517b6cf3a09661bf0d1
CandeGod <Candebeatriz14@gmail.com>
2024-09-11 12:43:26 -0600

    Ya quedo la conexion a la bd

---
+++ Connection/DatabaseConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapacheBigoton.Connection
+{
+    public  class DatabaseConnection
+    {
+        public SqlConnection GetConnection()
+        {
+            //Cambiar unicamente el nombre de su servidor de la base de datos de sql y tener la bd creada
+            SqlConnection connection = new SqlConnection("SERVER=DELL\\CANDE;DATABASE=DB_Barberias;Integrated Security=True;TrustServerCertificate=True;");
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
--- FrmInicio.cs
+++ FrmInicio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MapacheBigoton.Connection;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,25 @@ namespace MapacheBigoton
 {
     public partial class FrmInicio : Form
     {
+        private DatabaseConnection _databaseConnection;
         public FrmInicio()
         {
             InitializeComponent();
+            _databaseConnection = new DatabaseConnection();
+        }
+
+        private void FrmInicio_Load(object sender, EventArgs e)
+        {
+            var isConnected = _databaseConnection.GetConnection();
+            if (isConnected != null && isConnected.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Conexión a la base de datos exitosa.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("La conexion no se establecio");
+            }
+
         }
     }
 }
