1f909ed9f1cbe0d2f4a46aa2c1963e139ec84d1c
Angel031010 <angel10diez03@gmail.com>
2024-09-16 20:15:49 -0600

    Ahora es posible agregar mas servicios

--- Program.cs
+++ Program.cs
@@ -11,11 +11,7 @@ namespace MapacheBigoton
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
             Application.Run(new FrmServicios());
-=======
-            Application.Run(new frmAgendar());
->>>>>>> origin/master
         }
     }
 }
